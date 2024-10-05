b7f4f6fef9297ec3e62f443e693823e36410c5ab
Kumazawa <kumazawa@ic.idofront.com>
2024-10-06 01:56:28 +0900

    Parser/Dumper の Register/Resolve メソッドを実装

--- proj/plugincontainer/src/PluginContainer.cpp
+++ proj/plugincontainer/src/PluginContainer.cpp
@@ -1,15 +1,51 @@
 #include <PluginContainer.hpp>
 #include <exception/NotImplementedException.hpp>
 
+#include <Utility.hpp>
+#include <boost/format.hpp>
+#include <spdlog/spdlog.h>
+
 namespace PluginContainer
 {
-void PluginContainer::RegisterDumper(const std::string &type, PluginContract::Dumper::DumperPtr dumper)
+void PluginContainer::RegisterDumper(PluginContract::Dumper::DumperPtr dumper)
 {
-    throw Utility::Exception::NotImplementedException("Not implemented");
+    if (!dumper)
+    {
+        throw std::invalid_argument("Dumper is null.");
+    }
+
+    auto &dumperRef = *dumper;
+    const auto &type = typeid(dumperRef);
+    auto type_name = Utility::Demangle(type.name());
+
+    auto dumperPtr = this->ResolveDumper<PluginContract::Dumper::AbstractDumper>();
+
+    if (dumperPtr != nullptr)
+    {
+        SPDLOG_WARN(boost::str(boost::format("Dumper {} already registered.") % type_name));
+    }
+    SPDLOG_INFO(boost::str(boost::format("Registered dumper: {}") % type_name));
+    _Dumpers.push_back(dumper);
 }
 
-void PluginContainer::RegisterParser(const std::string &type, PluginContract::Parser::ParserPtr parser)
+void PluginContainer::RegisterParser(PluginContract::Parser::ParserPtr parser)
 {
-    throw Utility::Exception::NotImplementedException("Not implemented");
+    if (!parser)
+    {
+        throw std::invalid_argument("Parser is null.");
+    }
+
+    auto &parserRef = *parser;
+    const auto &type = typeid(parserRef);
+    auto type_name = Utility::Demangle(type.name());
+
+    auto parserPtr = this->ResolveParser<PluginContract::Parser::AbstractParser>();
+
+    if (parserPtr != nullptr)
+    {
+        SPDLOG_WARN(boost::str(boost::format("Parser {} already registered.") % type_name));
+    }
+    SPDLOG_INFO(boost::str(boost::format("Registered parser: {}") % type_name));
+    _Parsers.push_back(parser);
 }
 } // namespace PluginContainer
--- proj/plugincontainer/src/PluginContainer.hpp
+++ proj/plugincontainer/src/PluginContainer.hpp
@@ -4,6 +4,7 @@
 #include <Dumper/AbstractDumper.hpp>
 #include <Parser/AbstractParser.hpp>
 #include <memory>
+#include <vector>
 
 namespace PluginContainer
 {
@@ -13,8 +14,27 @@ class PluginContainer
     PluginContainer() = default;
     virtual ~PluginContainer() = default;
 
-    void RegisterDumper(const std::string &type, PluginContract::Dumper::DumperPtr dumper);
-    void RegisterParser(const std::string &type, PluginContract::Parser::ParserPtr parser);
+    void RegisterDumper(PluginContract::Dumper::DumperPtr dumper);
+    template <class T> PluginContract::Dumper::DumperPtr ResolveDumper()
+    {
+        auto dumperItr = std::find_if(_Dumpers.begin(), _Dumpers.end(), [](PluginContract::Dumper::DumperPtr dumper) {
+            return dynamic_cast<T *>(dumper.get()) != nullptr;
+        });
+        return dumperItr != _Dumpers.end() ? *dumperItr : nullptr;
+    }
+
+    void RegisterParser(PluginContract::Parser::ParserPtr parser);
+    template <class T> PluginContract::Parser::ParserPtr ResolveParser()
+    {
+        auto parserItr = std::find_if(_Parsers.begin(), _Parsers.end(), [](PluginContract::Parser::ParserPtr parser) {
+            return dynamic_cast<T *>(parser.get()) != nullptr;
+        });
+        return parserItr != _Parsers.end() ? *parserItr : nullptr;
+    }
+
+  private:
+    std::vector<PluginContract::Dumper::DumperPtr> _Dumpers;
+    std::vector<PluginContract::Parser::ParserPtr> _Parsers;
 };
 
 using PluginContainerPtr = std::shared_ptr<PluginContainer>;
