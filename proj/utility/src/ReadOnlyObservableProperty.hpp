062c1a7777700fb911ea4d48caf73a1247edff7c
Kumazawa <kumazawa@ic.idofront.com>
2024-10-05 11:56:28 +0900

    ReadOnlyObservableProperty を実装

--- proj/utility/src/ReadOnlyObservableProperty.hpp
+++ proj/utility/src/ReadOnlyObservableProperty.hpp
@@ -5,7 +5,41 @@
 
 namespace Utility
 {
-// TODO ReadOnlyObservableProperty を実装する
+template <typename T> class ReadOnlyObservableProperty;
+template <typename T> using ReadOnlyObservablePropertyPtr = std::shared_ptr<ReadOnlyObservableProperty<T>>;
+
+/// @brief 値の変更通知を提供可能な読み取り専用のプロパティ
+/// @tparam T 値の型
+/// @details コンストラクタで ObservableProperty を受け取り，そのインスタンスの変化を通知する．
+template <typename T> class ReadOnlyObservableProperty
+{
+  public:
+    ReadOnlyObservableProperty() = delete;
+    ReadOnlyObservableProperty(ObservablePropertyPtr propertyPtr)
+    {
+        if (propertyPtr == nullptr)
+        {
+            throw std::runtime_error("Null pointer is not allowed.");
+        }
+
+        _PropertyPtr = propertyPtr;
+    }
+
+    T Value() const
+    {
+        return _Property->Value();
+    }
+
+    /// @brief 値が変更されたときに呼ばれるコールバックを登録する．
+    /// @param callback コールバック
+    void RegisterCallback(std::function<void(T, T)> callback)
+    {
+        _Property->RegisterCallback(callback);
+    }
+
+  private:
+    ObservablePropertyPtr _PropertyPtr;
+};
 } // namespace Utility
 
 #endif
