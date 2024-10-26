ceccabead9fc885792e6d01e48a9481c02db6e0c
Kumazawa <kumazawa@ic.idofront.com>
2024-10-27 02:09:46 +0900

    現実装では Ethernet Type として VLAN を指定されると動作しないため警告メッセージを出力するよう修正

--- plugin/EthernetPlugin/src/Packet/Ethernet.cpp
+++ plugin/EthernetPlugin/src/Packet/Ethernet.cpp
@@ -44,6 +44,21 @@ void Ethernet::OnTypeChanged(uint16_t newValue, uint16_t oldValue)
 {
     auto header = this->Header();
     header->ether_type = htons(newValue);
+
+    switch (newValue)
+    {
+    case ETHERTYPE_IP:
+    case ETHERTYPE_ARP:
+    case ETHERTYPE_REVARP:
+    case ETHERTYPE_IPV6:
+        break;
+    case ETHERTYPE_VLAN:
+        SPDLOG_WARN("Ethernet Type: VLAN is not supported. Probably, this packet could not be parsed.");
+        break;
+    default:
+        SPDLOG_INFO("Unknown Ethernet Type: 0x{:04x} is set.", newValue);
+        break;
+    }
 }
 
 void Ethernet::OnPayloadLengthChanged(uint16_t newValue, uint16_t oldValue)
